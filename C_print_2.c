78fffa6f6b6db210d83aa87869b57072d341c9cf
SEYEON_PARK <fourleaf123@naver.com>
2020-08-29 00:27:56 +0900

    print 연습!(2) 자료형에 맞는 형식 지정자를 사용하여 출력하기.

---
+++ C_print_2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 10, b = 2;
+	char c = 'G';
+	float d = 1.3;
+	double e = 1.5;
+
+	printf("%d \n%d \n%c \n%f \n%lf \n", a, b, c, d, e);
+	return 0;
+}
